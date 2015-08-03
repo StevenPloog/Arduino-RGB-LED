9e5f43f50006c37762d9a126a675195bb1ec3fd6
Steven <emailstevenp@gmail.com>
2015-08-03 13:06:53 -0400

    Replace tabs with 4 spaces. Cleanup and add newline at end of file

--- RGBLED.cpp
+++ RGBLED.cpp
@@ -4,91 +4,92 @@
 // Constructor for the led
 // Pins passed to this constructor must implement Arduino's analogWrite method
 RGBLED::RGBLED(int redPin, int greenPin, int bluePin) {
-	this.redPin = redPin;
-	this.greenPin = greenPin;
-	this.bluePin = bluePin;
+    this.redPin = redPin;
+    this.greenPin = greenPin;
+    this.bluePin = bluePin;
 
-	red = 0;
-	green = 0;
-	blue = 0;
+    red = 0;
+    green = 0;
+    blue = 0;
 }
 
 // Fade the red, green and blue values
 void RGBLED::fadeRGB(int r, int g, int b) {
-	while (red != r || green != g || blue != b) {
-		if (red < r)		{ setRed(red+1); }
-		else if (red > r)	{ setRed(red-1); }
+    while (red != r || green != g || blue != b) {
+        if (red < r)        { setRed(red+1); }
+        else if (red > r)   { setRed(red-1); }
 
-		if (green < g)		{ setGreen(green+1); }
-		else if (green > g)	{ setGreen(green-1); }
+        if (green < g)      { setGreen(green+1); }
+        else if (green > g) { setGreen(green-1); }
 
-		if (blue < b)		{ setBlue(blue+1); }
-		else if (blue > b)	{ setBlue(blue-1); }
+        if (blue < b)       { setBlue(blue+1); }
+        else if (blue > b)  { setBlue(blue-1); }
 
-		delay(RGBLED_FADE_DELAY);
-	}
+        delay(RGBLED_FADE_DELAY);
+    }
 }
 
 // Fade the red value
 void RGBLED::fadeRed(int r) {
-	while (red != r) {
-		if (red < r) { setRed(red+1); }
-		else { setRed(red-1); }
+    while (red != r) {
+        if (red < r)    { setRed(red+1); }
+        else            { setRed(red-1); }
 
-		delay(RGBLED_FADE_DELAY);
-	}
+        delay(RGBLED_FADE_DELAY);
+    }
 }
 
 // Fade the green value
 void RGBLED::fadeGreen(int g) {
-	while (green != g) {
-		if (green < g) { setGreen(green+1); }
-		else { setGreen(green-1); }
+    while (green != g) {
+        if (green < g)  { setGreen(green+1); }
+        else            { setGreen(green-1); }
 
-		delay(RGBLED_FADE_DELAY);
-	}
+        delay(RGBLED_FADE_DELAY);
+    }
 }
 
 // Fade the blue value
 void RGBLED::fadeBlue(int b) {
-	while (blue != b) {
-		if (blue < b) { setblue(blue+1); }
-		else { setblue(blue-1); }
+    while (blue != b) {
+        if (blue < b)   { setblue(blue+1); }
+        else            { setblue(blue-1); }
 
-		delay(RGBLED_FADE_DELAY);
-	}
+        delay(RGBLED_FADE_DELAY);
+    }
 }
 
 // Set the red, green and blue values
 void RGBLED::setRGB(int r, int g, int b) {
-	red = r;
-	green = g;
-	blue = b;
+    red = r;
+    green = g;
+    blue = b;
 
-	analogWrite(redPin, red);
-	analogWrite(greenPin, green);
-	analogWrite(bluePin, blue);
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
 }
 
 // Set the red value
 void RGBLED::setRed(int r) {
-	red = r;
-	analogWrite(redPin, red);
+    red = r;
+    analogWrite(redPin, red);
 }
 
 // Set the green value
 void RGBLED::setGreen(int g) {
-	green = g;
-	analogWrite(greenPin, green);
+    green = g;
+    analogWrite(greenPin, green);
 }
 
 // Set the blue value
 void RGBLED::setBlue(int b) {
-	blue = b;
-	analogWrite(bluePin, blue);
+    blue = b;
+    analogWrite(bluePin, blue);
 }
 
 // Getter methods for rgb values
-int RGBLED::red()	{ return red;	}
-int RGBLED::green()	{ return green;	}
-int RGBLED::blue()	{ return blue;	}
+int RGBLED::red()   { return red;   }
+int RGBLED::green() { return green; }
+int RGBLED::blue()  { return blue;  }
+
--- RGBLED.h
+++ RGBLED.h
@@ -6,30 +6,31 @@
 #endif
 
 class RGBLED {
-	public:
-		RGBLED(int redPin, int greenPin, int bluePin);
+    public:
+        RGBLED(int redPin, int greenPin, int bluePin);
 
-		void fadeRGB(int r, int g, int b);
-		void fadeRed(int r);
-		void fadeGreen(int g);
-		void fadeBlue(int b);
+        void fadeRGB(int r, int g, int b);
+        void fadeRed(int r);
+        void fadeGreen(int g);
+        void fadeBlue(int b);
 
-		void setRGB(int r, int g, int b);
-		void setRed(int r);
-		void setGreen(int g);
-		void setBlue(int b);
+        void setRGB(int r, int g, int b);
+        void setRed(int r);
+        void setGreen(int g);
+        void setBlue(int b);
 
-		int red();
-		int green();
-		int blue();
+        int red();
+        int green();
+        int blue();
 
-	private:
-		int redPin;
-		int greenPin;
-		int bluePin;
-		int red;
-		int green;
-		int blue;
+    private:
+        int redPin;
+        int greenPin;
+        int bluePin;
+        int red;
+        int green;
+        int blue;
 }
 
 #endif
+
