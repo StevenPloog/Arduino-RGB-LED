4da4b747f84b7f8be0f6480dcdf999a7177e8367
Steven <emailstevenp@gmail.com>
2015-08-03 13:02:12 -0400

    Added fade functionality

--- RGBLED.cpp
+++ RGBLED.cpp
@@ -13,6 +13,52 @@ RGBLED::RGBLED(int redPin, int greenPin, int bluePin) {
 	blue = 0;
 }
 
+// Fade the red, green and blue values
+void RGBLED::fadeRGB(int r, int g, int b) {
+	while (red != r || green != g || blue != b) {
+		if (red < r)		{ setRed(red+1); }
+		else if (red > r)	{ setRed(red-1); }
+
+		if (green < g)		{ setGreen(green+1); }
+		else if (green > g)	{ setGreen(green-1); }
+
+		if (blue < b)		{ setBlue(blue+1); }
+		else if (blue > b)	{ setBlue(blue-1); }
+
+		delay(RGBLED_FADE_DELAY);
+	}
+}
+
+// Fade the red value
+void RGBLED::fadeRed(int r) {
+	while (red != r) {
+		if (red < r) { setRed(red+1); }
+		else { setRed(red-1); }
+
+		delay(RGBLED_FADE_DELAY);
+	}
+}
+
+// Fade the green value
+void RGBLED::fadeGreen(int g) {
+	while (green != g) {
+		if (green < g) { setGreen(green+1); }
+		else { setGreen(green-1); }
+
+		delay(RGBLED_FADE_DELAY);
+	}
+}
+
+// Fade the blue value
+void RGBLED::fadeBlue(int b) {
+	while (blue != b) {
+		if (blue < b) { setblue(blue+1); }
+		else { setblue(blue-1); }
+
+		delay(RGBLED_FADE_DELAY);
+	}
+}
+
 // Set the red, green and blue values
 void RGBLED::setRGB(int r, int g, int b) {
 	red = r;
--- RGBLED.h
+++ RGBLED.h
@@ -1,9 +1,19 @@
 #ifndef RGBLED_H
 #define RGBLED_H
 
+#ifndef RGBLED_FADE_DELAY
+#define RGBLED_FADE_DELAY 15
+#endif
+
 class RGBLED {
 	public:
 		RGBLED(int redPin, int greenPin, int bluePin);
+
+		void fadeRGB(int r, int g, int b);
+		void fadeRed(int r);
+		void fadeGreen(int g);
+		void fadeBlue(int b);
+
 		void setRGB(int r, int g, int b);
 		void setRed(int r);
 		void setGreen(int g);
